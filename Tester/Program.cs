64d008354bdeb0def40de116b7efb6970a742cb8
Christopher Demicoli <christopher.demicoli@gaminginnovationgroup.com>
2016-03-29 01:09:47 +0200

    more testing

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -8,6 +8,7 @@ namespace Tester
 {
     class Program
     {
+        //01 00 00 38 0f 40 3f 94 73 97 80 57 bd 83 d5 cb 98 f4 22 7a 01 06 6e 65 6d 6f 02 12 0d be 70 8d 93 d4 13 ce 31 96 e4 3f 78 2a 0a ee 04 06 c0 a8 01 10 05 06 00 00 00 03 
         static void Main(string[] args)
         {
             IPEndPoint anySender = new IPEndPoint(IPAddress.Any, 0);
